fcc83c2b3bb0a1fcd130ff79331a4e05ec0a788c
NicholasLi <nicholas@lifamily.ca>
2024-02-03 17:33:30 -0500

    created training method

---
+++ training.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import os
+from mediapipe_model_maker import gesture_recognizer
+import matplotlib.pyplot as plt
+
+datasetPath = r"C:\Users\Nicholas\Downloads\asl_alphabet_train"
+labels = [] 
+for i in os.listdir(datasetPath):
+    if os.path.isdir(os.path.join(datasetPath)):
+        labels.append(i)
+print(labels)
+
+data = gesture_recognizer.Dataset.from_folder(
+    dirname=datasetPath,
+    hparams=gesture_recognizer.HandDataPreprocessingParams()
+)
+
+train_data, rest_data = data.split(0.8)
+validation_data, test_data = rest_data.split(0.5)
+
+
+hparams = gesture_recognizer.HParams(export_dir="ASL_model")
+options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
+model = gesture_recognizer.GestureRecognizer.create(
+    train_data=train_data,
+    validation_data=validation_data,
+    options=options
+)
+
+loss, acc = model.evaluate(test_data, batch_size=1)
+print(f"Test loss:{loss}, Test accuracy:{acc}")
+
+
+
+
